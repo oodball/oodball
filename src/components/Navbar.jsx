28372cd76d398a7411dc45868a7fc096785e04bc
oodball <audreyya03@gmail.com>
2025-06-02 13:49:23 -0700

    testing nav dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="navbar-brand">
         <Link to="/">oodball</Link>
       </div>
       <div className="navbar-links">
-        <Link to="/">Oodball</Link>
-        <Link to="/Foodball">Foodball</Link>
-        <Link to="/Filmball">Filmball</Link>
-        <Link to="/Embroodball">Embroodball</Link>
+        <div className="dropdown">
+          <button 
+            className="dropdown-button"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            Menu ▼
+          </button>
+          {isOpen && (
+            <div className="dropdown-content">
+              <Link to="/">Oodball</Link>
+              <Link to="/Foodball">Foodball</Link>
+              <Link to="/Filmball">Filmball</Link>
+              <Link to="/Embroodball">Embroodball</Link>
+            </div>
+          )}
+        </div>
       </div>
     </nav>
   );

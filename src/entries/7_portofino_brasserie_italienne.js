d016696b0dc785232da599ee76e1577c15022b9b
oodball <audreyya03@gmail.com>
2025-07-30 15:54:21 -0700

    remove ghpages deployment

---
+++ src/entries/7_portofino_brasserie_italienne.js
@@ -0,0 +1,22 @@
+export const portofino_brasserie_italienne = {
+    id: 7, 
+    title: "Portofino Brasserie Italienne",
+    content: `
+    Date ea
+    ## INTRODUCTION
+    
+    ## FOOD
+  
+    
+  
+    ## OVERALL THOUGHTS
+  
+    
+    
+    `,
+    date: "7/30/2025", 
+    timestamp: "2025-07-30T20:45:00.000Z",
+    rating: 2.9,
+    location: "San Diego",
+    tags: ["Italian", "Tiramisu", "Pasta", "Ocean Beach"] 
+  };

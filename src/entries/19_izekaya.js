96bcb081a245ddf2ca319d52446205d825f60ba7
oodball <audreyya03@gmail.com>
2025-09-30 12:24:42 -0700

    izekaya

--- src/entries/19_izekaya.js
+++ src/entries/19_izekaya.js
@@ -2,49 +2,75 @@ export const xu_le_izekaya = {
     id: 19, 
     title: "Xu Le Izekaya 緒樂酒場",
     content: `## INTRODUCTION
-    
-    ## FOOD
-    ***Watercress? with mushrooms***
-    my favorite dish of the night
-    perfect texture
-    easy to grab
-    slightly sour taste to them which was amazing
-    would go back just for this dish
 
-    4/5
+I'm writing again to stop overthinking again so you guys are getting a new article while I'm supposed to be learning about feature vectors and linear regression again. 
 
-    ***mentaiko udon***
-    very very good also
-    might be too rich if i had all the sauce on its own
-    alot of sauce
-    mentaiko good
-    suprisingly not salty
+I want to write about this place because it was another memorable place that I visited while in Taiwan. I was craving skewers before we went to G Star, and this place was decently close to where we were staying.
 
-    3.5/5
+This izekaya closely resembled the izekayas you see in Japan. Figures considering the history but I felt like I had to say that anyways. The vibe instantly felt more vibrant, as if the place was inviting you to come in and rest for a bit. 
 
-    ***Skewers***
+Pretty dangerous if you're trying not to drink. 
 
-    beef one was alright, kinda chewy
+I didn't drink, but that's because I knew I would be drinking later. I would have caved otherwise.
 
-    chickens:
-    one with the sauce on it was good, kept falling off though so it was a bit tiring to eat 
+Maybe we'll find out next time I go.
 
-    salt was my favorite, very juicy chicken. had this crisp layer to it that i thought was really nice
+## FOOD
 
-    tbh i liked the ones at hinotez more
+![Xu Le Izekaya](/images/Foodball/xu_le_izekaya/xule_izekaya.JPG "One Day I'll Know What Veggies We Ate")
 
-    2.5/5
-    
-    
-    ## OVERALL THOUGHTS
-    
-    
+***Veggies with Mushrooms***
+
+This was by far my favorite dish of the night. I still don't know what to call this dish because I don't actually know what it's called, but I know I'd come back just for this dish. 
+
+The veggies are native to Taiwan, and they're special because it rains so much that the inside hollow part of the veggie is actually bigger than in other parts of the world. They were so crunchy and had the perfect texture when crunching down. They were cut just perfectly that each time you went to grab the veggies, there weren't any that were suddenly falling off or those veggies that are too long and get tangled with all the other veggies in the plate.
+
+The taste was also perfect for me. There was this creaminess, yet also a very distinct sour taste that was refreshing. The structure of the veggies also made it so that they soaked up more of the juice. I almost requested another plate, but the portion size was so big that it was enough for the 3 of us. 
+
+I still think about this dish a month later. 
+
+5/5
+
+***Mentaiko Udon***
+
+The mentaiko udon was also another favorite dish that I had that night. The udon was thick enough to hold a significant amount of sauce on it, and the sauce itself was very creamy. I was expecting the dish to be super salty considering mentaiko is usually salty, but it was pleasantly balanced out by the cream. 
+
+It was pretty rich, but it was a dish that I couldn't stop eating because the flavor was too good. 
+
+However, since I was able to split it with two other people, it was the perfect amount. 
+
+I'm not sure I would have been able to finish it if I had ordered it for myself. There was alot of leftover sauce that I couldn't finish, but someone else was able to finish it for me instead.
+
+This is why I never go out to eat by myself.
+
+4.5/5
+
+***Skewers***
+
+The skewers were mid. 
+
+The beef skewer was kinda chewy, but the taste was alright. There wasn't anything memorable about it. 
+
+THe chicken skewers were very juicy, and had a crisp layer to them that was really nice to eat. It's almost like a salt crust, except the crust took off the first layer of skin and left really tender chicken?
+
+I realize that I prefer my skewers very simple. I don't like the ones with a sauce on top because they just feel too messy to eat. I don't want to have to worry about things falling off a skewer if they're supposed to be a street food you can easily eat while walking. 
+
+The best tasting skewers are the ones with the chili dry rub or just salt.
+
+These skewers were alright, but I've had better elsewhere.
+
+2.5/5
+
+
+## OVERALL THOUGHTS
+
+I recommend this place. The vibe in this izekaya was so cozy. There aren't many seats, so they have outdoor seating right next to the kitchen area. 
     
     `,
-    date: "7/2/2025", 
-    timestamp: "2025-07-02T20:45:00.000Z",
-    rating: 2.9,
+    date: "9/30/2025", 
+    timestamp: "2025-09-30T12:24:00.000Z",
+    rating: 4.0,
     location: "Taipei",
-    tags: ["Japanese", "Izekaya", "Yakitori", "Skewers", "Mentaiko Pasta", "Watercress"],
-    published: false
+    tags: ["Japanese", "Izekaya", "Yakitori", "Skewers", "Mentaiko Pasta", "Watercress?"],
+    published: true
     };

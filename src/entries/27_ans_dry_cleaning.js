8461dca6dfd0faba041500879d47bf6a8cb7ce24
oodball <audreyya03@gmail.com>
2026-01-27 18:11:04 -0800

    an's

--- src/entries/27_ans_dry_cleaning.js
+++ src/entries/27_ans_dry_cleaning.js
@@ -5,45 +5,80 @@ export const ans_dc = {
 
 ## INTRODUCTION
 
-I love ice cream. I love San Diego. I love being able to eat ice cream in December. 
+I love ice cream. I love San Diego. I love being able to eat ice cream in December. So why haven't I gone to any An's franchises?
 
-Somehow even with all that I've never been to any of the An's franchises.
+Good question.
 
-Considering they're #1 in the country for their ice cream, I finally had an excuse to go to An's after using my precious zoo membership and scheming to make as many people as possible get a membership.
+I finally had an excuse to go to the #1 ice cream shop in the country after using my precious zoo membership and scheming to make as many people as possible get a membership.
 
 Current Pyramid Scheme Success Count: 4
 
-We specifically went to An's Dry Cleaning because of their Tongue Pop 
+We specifically went to An's Dry Cleaning because of their Tongue Pop flavor. I thought all An's locations had the same flavors, but apparently they like being exclusive. 
+
+I'm a little put-off by the fact that you have to wait in line for over an hour before you get to buy your ice cream. That alone is pretty impressive because it was kind of cold out, and yet the line went around the building, so that has to say something about the quality of ice cream I suppose.
+
+Something interesting about them is that they walk you through all the flavors before you actually get to purchasing any ice cream. I always feel super bad about asking for more than 2 samples at a shop, so being forced to taste everything before making a decision works out for me.
+
+But I do understand why it has the line that it does now...
+
     
 ## FOOD
 
-![spread](/images/Foodball/ans_dc/spread.jpg "Yeah Who Takes Pics w/Flash in the Dark Anyways")
+![spread](/images/Foodball/ans_dc/spread.jpg "People Who Use Flash in the Dark Are Assholes")
+
+
+***Palette Cleanser***
+
+This isn't even technically one of their flavors. They hand this to you right before they take you through all their flavors, but the flavor of this left the biggest impression on me. 
+
+The lime and ginger instantly hits a spot that you didn't know you needed until that moment. I was pleasantly surprised by the mild burn of the ginger in my throat, and I knew that something had to go along with it to balance out the sourness from the lime. 
+
+However, I didn't really care for the texture of the ice cream. Even with this and all the other ones I tried, I felt like there was something off about the consistency.
+
+Will I be crucified if I say that I prefer Stella Jean's consistency more?
+
+3/5
 
 ***Tongue Pop***
 
 I was immediately interested in this flavor when looking it up online. It may or may not have been the reason we went to this location instead of Hatmaker's in Del Mar. 
 
+I really like light ice creams. I tend towards more sorbet fruity flavors than actual creamy flavors since I like how light it feels, and usually I'm eating it after having a meal, so I can't eat that much anyways. 
 
-Instnatly, Mango, Raspberry, and Mint
+When they let me try this immediately after the palette cleanser, I knew that this was the combo I had to get. 
 
-***Palette Cleanser***
+I couldn't really taste the mango in it which is a shame, but I really liked the raspberry and mint combo.
 
-Lime and Ginger
+Much like the Palette Cleanser though, the consistency of this was less sorbet, and more Italian ice though. I would have preferred my ice cream to not be as stiff. 
 
-This isn't even technically one of their flavors. They hand this to you right before they take you through la
+3/5
+
 
 ***Hot Apple Cider***
 
+This one is a season specialty since it was around Christmastime. I was really craving a drink from somewhere, and that craving only increased while I was in line...
+
+You can tell instantly that the cider is house-made. The scent of apples is insane, and when paired wtih the my ice cream, it was a beautiful balance. 
+
+I think that the cider was personally too sweet, but it was very nice when pairing with my sour ice cream. 
+
+3.5/5
 
 ## OVERALL THOUGHTS
+
+I think this place is good to try once or twice. I like that they're constantly coming up with new flavors that surprisingly work well together, and the concept of their shops are really nice. 
+
+I'm not sure if I'm willing to wait that long in line each time just to get my ice cream fix though. 
+
+They are very on theme, and I appreciate the level of thought and care into their marketing, but their ice cream is also pretty expensive now that I think of it. It was pretty good, but at the same time, it felt like if I were purchasing a pint for the price they were selling it at, I better be finding gold in it. 
         
         
         
         `,
-        date: "1/7/2026", 
-        timestamp: "2026-01-07T02:25:00.000Z",
-        rating: 2.9,
+        date: "1/27/2026", 
+        timestamp: "2026-01-27T18:00:00.000Z",
+        rating: 3.2,
         location: "San Diego",
-        tags: ["Ice Cream", "Apple Cider", "Dessert", "Downtown"], 
-        published: false
+        tags: ["Ice Cream", "Tongue Pop", "Sorbet", "Palette Cleanser", "Apple Cider", "Dessert", "Downtown"], 
+        published: true
         };

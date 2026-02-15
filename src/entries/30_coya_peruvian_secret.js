b3f95c7434d5534c3d5f4ed4674b66c3494096b2
oodball <audreyya03@gmail.com>
2026-02-15 15:03:14 -0800

    coya

--- src/entries/30_coya_peruvian_secret.js
+++ src/entries/30_coya_peruvian_secret.js
@@ -1,21 +1,65 @@
 export const coya = {
     id: 30, 
     title: "Coya Peruvian Secret",
-    content: `
-    ## INTRODUCTION
-        
-    ## FOOD
+    content: `## INTRODUCTION
+
+It took us a long time to get to this restaurant. Originally, we planned on booking it for my parent's anniversary, which we had to cancel because the rains were too bad, and then some time passed, and we finally booked it for a birthday. 
+
+Peruvian food isn't really something that I've had too much before. I know that it's somewhat similar to Chinese cuisine for some reason, so my parents really liked it, but I haven't had the chance to go get it before.
     
+## FOOD
+
+***Ceviche Clasico***
+
+![ceviche](/images/Foodball/coya/ceviche.jpg "Rainbow Fish")
+
+This was a pretty good ceviche. The portion was a bit small to share for 4 people, but it was an appetizer so maybe it intended to do that. 
+
+What this dish did really well was prepare us for the rest of the dishes to come. The ceviche was super sour and refreshing, which made me crave more food after I finished it off. The chickpeas in the dish gave a nice contrasting texture to the fish, and also helped balance out the sourness from the dressing. 
+
+
+4/5
+
+***Pork Belly***
+![pork belly](/images/Foodball/coya/porkbelly.jpg "Peppa")
+
+I don't think I can eat this all by myself. I'm not a big fan of super fatty meats. Pork belly tastes really good, but I can only have so much before it starts feeling like I'm just chugging oil. I've recently realized my tolerance for oily foods has gone down. 
+
+This is what aging does to you...
+
+I think there was apple in this dish which contrasted nicely to the soft pork belly.
+
+3.5/5
+
+***Half-Roasted Chicken***
+![chicken](/images/Foodball/coya/chicken.jpg "Chicken Little")
+
+I don't really have words for this because it felt like a normal chicken? It was a little dry compared to all the dishes, but the rice underneath it though was pretty good. There was a specific flavor to it that I can't quite name, but I thought that it paired nicely with the chicken. 
+
+3/5
+
+***Lomo Saltado***
+
+![lomo saltado](/images/Foodball/coya/lomo_saltado.jpg "Miltank")
+
+The flavor on this was so good. I think that without the tomatoes, the beef might be a little dry, but since the acidity from the tomatoes cut through the beef a bit, and also the potatoes balanced out the sauce, this was a pretty good dish overall. 
+
+I don't know if the dish needed both rice and potatoes, but it didn't really add or take away any value.
+
+
+3.5/5
+
+
+## OVERALL THOUGHTS
+
+This was a pretty consistent restaurant. All of the dishes were around the same level, and none of them outshined each other. I would come back.
     
-    ## OVERALL THOUGHTS
-        
-        
         
         `,
-        date: "1/16/2026", 
-        timestamp: "2026-01-16T02:02:00.000Z",
-        rating: 2.5,
+        date: "2/15/2026", 
+        timestamp: "2026-02-15T21:50:00.000Z",
+        rating: 3.5,
         location: "San Diego",
         tags: ["Peruvian", "Pork Belly", "Lomo Saltado", "Ceviche Clasico", "Half-Roasted Baby Chicken", "Del Mar"], 
-        published: false
+        published: true
         };

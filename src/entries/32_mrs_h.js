6f41d170446539d3ded9e2cb6f71e39036399cb8
oodball <audreyya03@gmail.com>
2026-03-01 14:08:32 -0800

    mrs. h

---
+++ src/entries/32_mrs_h.js
@@ -0,0 +1,50 @@
+export const mrs_h = {
+    id: 32, 
+    title: "Mrs. H's Coffee House",
+    content: `
+## INTRODUCTION
+Taking a hike up Hollywood Rd. in Central is very rarely worth the hike. Even if it were the middle of winter, I usually try to avoid taking the hike since it's just torture. Whoever called Hong Kong a walkable city clearly has never seen a walkable city before.
+
+And yet we did it in the middle of summer. 
+
+This place is pretty interesting considering it's a coffee shop by day, and a bar by night. Also, they exclusively speak in English, which seems to be common among bars in Asia for some reason. Does speaking English somehow make the place more elevated? Or do we just look that exotic?
+
+This bar was very nicely decorated inside, and even the bathrooms were themed, which set my expectations high. We noticed a pattern that the more the bathrooms match the vibe of the bar, the better their drinks were.
+
+## FOOD
+
+***Drinks***
+![round 1](/images/Foodball/mrs_h/round1.JPG "Looks Like A Bitch Cup Lowkey")
+I can't really describe the drink I had. It had a Dorito in it, and tasted like mango salsa? I wanted to try it out of curiosity. So far from the other drinks I've had in Hong Kong, I've been only mildly impressed compared to other bars, so I've just been in a habit of getting the most interesting drink. 
+
+Also, I noticed that all these bars tend to really like giving me some type of plant in my drink (See Tree from Penicillin), so when this came out with a sprig of cilantro, I was a little disappointed.
+
+But I was actually very happily surprised. The cocktail tasted basically like drinking salsa, and there was a perfect blend of flavor and alcohol aftertaste. There was a certain tangyness from the mango, and it was something I could drink easily. 
+
+![round 2](/images/Foodball/mrs_h/round2.JPG "Too Tipsy This is All You Get")
+
+And since this place was so good, we even went for round 2! Which if you know me is something I usually never do since I'm not a big drinker.
+
+But the vibes were just too good, I couldn't resist.
+
+I forgot exactly what I got in my second drink, but it was a customized drink according to a sheet the bartender has you fill out and he makes a drink he think you'll like. 
+
+It takes skill to be a good bartender. This man made me a drink so good I don't even remember anything but the feeling that he got my taste well. He hit the sourness and refreshing notes just right, and even managed to put in flavors I didn't mention, but he was able to deliver perfectly. 
+
+Thank you mystery bartender.
+
+4.5/5
+
+## OVERALL THOUGHTS
+
+Was it the place that was enjoyable, or the people I went with? The hike up Hollywood Rd was well worth for coming to Mrs. H's Coffee House. The drinks were interesting and nice, and the bartenders were super friendly. I felt comfortable staying here, unlike some other bars where the atmosphere is a little more serious and pressuring. 
+
+![vibe](/images/Foodball/mrs_h/love.JPG "Hootin n Hollerin In the Establishment")
+`,
+        date: "3/1/2026", 
+        timestamp: "2026-03-01T14:07:00.000Z",
+        rating: 4.5,
+        location: "Hong Kong",
+        tags: ["Central ", "Hollywood Rd", "Bar"], 
+        published: true
+        };

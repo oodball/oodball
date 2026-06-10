0f3c8fa0638f5c47e9a37e961f844266c1181c67
oodball <audreyya03@gmail.com>
2026-06-10 13:04:41 -0700

    mec

---
+++ src/entries/34_mec.js
@@ -0,0 +1,70 @@
+export const mec = {
+    id: 34, 
+    title: "Mediterannean Exploration Company",
+    content: `
+## INTRODUCTION
+Long time no see everybody. I'm feeling pretty low lately and Therese Dao and I keep talking about this one place we visited in Portland, so I thought it was time to revive Foodball.
+
+Mediterannean Exploration Company was one of the most recommended places in Portland (source: Reddit, Beli). It's a little out of the way from public transport, but walking there made us hungrier so maybe that was the intention. 
+
+Also, my SD card corrupted in the first two days of the trip, so I had to revert back to modern iPhone cameras. I also forgot to take pictures, so everybody thank Therese for keeping Foodball alive...
+
+## FOOD
+
+***Brussels Sprout Hummus***
+![hummus](/images/Foodball/mec/hummus.JPG "Licked the Plate")
+
+I'm not a big brussels sprouts person. I'll eat them, but I don't really go out of my way to order them most of the time since it doesn't really have that distinct of a taste to me. 
+
+But this hummus was really really good. The brussels sprouts were roasted just enough to have a nice char to them, which brought out a nice smoky flavor within the hummus.
+
+
+
+5/5
+
+***Chicken Meatballs***
+![falafel](/images/Foodball/mec/falafel.JPG "???")
+
+I don't really remember many details about how this tasted, but I know it ranked last on my list for food. 
+
+That doesn't mean it's bad, everything else was just too good for me to rank this higher than any other. 
+
+
+
+4/5
+
+***Lamb Chops***
+
+![lamb](/images/Foodball/mec/lamb.JPG "the GOAT")
+
+These lamb chops were really tender. The meat was tender and really flavorful in a way that I haven't really had from lamb chops before. I'm surprised, since normally I wouldn't get a plate that has only 2 pieces of lamb chop, but the intense level of flavor this singular piece of meat had makes me remember it almost 3 months later.
+
+
+
+5/5
+***Pistachio Buttermilk Cake***
+
+![cake](/images/Foodball/mec/cake.JPG "Motivation to Exercise")
+
+I took a walk to the bathroom and did jumping jacks just to fit this in my stomach. I was really excited for this since most places don't really have these types of cakes, but I was already so full from the rest of dinner. 
+
+The cake was nice and moist, and the pistachio gave it a nice sweetness that complemented the cake, which wasn't as sweet.
+4/5
+
+
+## OVERALL THOUGHTS
+
+When we first booked this trip, everybody kept on asking "What's in Portland?" and I never really had an answer for them. 
+
+Now that I'm back, I can confidently answer that Mediterranean Exploration Company is in Portland. I would go back just to eat at this place again if I had the chance. Everything was so consistently good, and there wasn't just one dish that stood out. 
+
+
+
+`,
+        date: "06/10/2026", 
+        timestamp: "2026-06-10T21:12:52.000Z",
+        rating: 4.5,
+        location: "Portland",
+        tags: ["Oregon", "Mediterranean","Hummus", "Lamb Chops", "Chicken Meatballs", "Pistachio Buttermilk Cake"], 
+        published: true
+        };

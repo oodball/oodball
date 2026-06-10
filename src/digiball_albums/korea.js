02a0605f6cdfc0d99c5b3373aeae1287c1ee83dd
oodball <audreyya03@gmail.com>
2026-06-10 15:05:16 -0700

    digiball captions 2

--- src/digiball_albums/korea.js
+++ src/digiball_albums/korea.js
@@ -1,12 +1,15 @@
 const photos = [
   {
-    "src": "/images/Digiball/korea/dumplings.JPG"
+    "src": "/images/Digiball/korea/dumplings.JPG",
+    "caption": "5 Min Before Finding Out The Restaurant Was Closed"
   },
   {
-    "src": "/images/Digiball/korea/hongdae.JPG"
+    "src": "/images/Digiball/korea/hongdae.JPG",
+    "caption": "Taken on Sony Cybershot Pro Max"
   },
   {
-    "src": "/images/Digiball/korea/korea.JPG"
+    "src": "/images/Digiball/korea/korea.JPG",
+    "caption": "Daehan Minguk Mansae"
   }
 ];
 
